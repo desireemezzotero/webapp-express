cc730775ddf2078c48eac6938109ed512134c9b3
desireemezzotero <desireemezzotero1@gmail.com>
2025-02-06 15:30:38 +0100

    [add] image

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,4 +1,5 @@
 const connection = require('../data/db')
+const imagePath = require('../middlewares/imagePath')
 
 const index = ('/', (req,res) => {
   const sql = 'SELECT * FROM movies'
@@ -13,18 +14,20 @@ const show = ('/', (req,res) => {
   const id = req.params.id;
   const sql = ` SELECT * FROM movies WHERE id = ? `
 
-  const sqlReviews = `SELECT ROUND(AVG(reviews.vote)) FROM reviews WHERE movie_id = ?`
+  const sqlReviews = `SELECT ROUND(AVG(reviews.vote)) Vote FROM reviews WHERE movie_id = ?`
 
   connection.query(sql, [id], (err, results) => {
     if(err) return res.status(500).json({err:'errore nella query del database'})
     if(results.length == 0) return res.status(400).json({err:'film non trovato'})
     const movie = results[0]
- 
 
     connection.query(sqlReviews, [id], (err, resultsReviews) => {
       if (err) return res.status(500).json({err:'errore nella query del database'})
         movie.reviews = resultsReviews;
-      res.json(movie)
+        const img = req.imagePath + movie.image
+      res.json({
+        ... movie,
+        img})
     }) 
   })
 })
